293877b18144f6ba11dc96af3c4cb05785f2146e
Rick <hanyoung.jeong@inbyu.com>
2021-01-06 10:41:02 +0900

    usage php-cs-fixer test on common.php

--- common.php
+++ common.php
@@ -1,9 +1,9 @@
 <?php
 
-define ("_PATH", __DIR__);
-define ("_LIB", _PATH."/lib");
+ini_set('error_reporting', E_ALL);
+
+define("_PATH", __DIR__);
+define("_LIB", _PATH."/lib");
 
 
 echo _PATH;
-
-?>
